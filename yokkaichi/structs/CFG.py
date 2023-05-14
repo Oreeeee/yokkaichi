08f1547d4b55656b381714ec01b50a979ac34434
Oreeeee <oree3ee@gmail.com>
2023-05-14 22:19:50 +0200

    Fix compatibility with older Python versions

--- yokkaichi/structs/CFG.py
+++ yokkaichi/structs/CFG.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 
 @dataclass
 class CFG:
-    platforms: list[str] = field(
+    platforms: list = field(
         default_factory=list
     )  # Possible values: "java", "bedrock"
     query_java: bool = field(default_factory=bool)
@@ -15,10 +15,10 @@ class CFG:
     masscan_args: str = field(default_factory=str)
     masscan_output: bool = field(default_factory=bool)
     masscan_output_location: str = field(default_factory=str)
-    masscan_country_list: list[str] = field(default_factory=list)
+    masscan_country_list: list = field(default_factory=list)
     masscan_ip_list: str = field(default_factory=str)
     ip_list: str = field(default_factory=str)
-    ports: list[int] = field(default_factory=list)
+    ports: list = field(default_factory=list)
     threads: int = field(default_factory=int)
     output: str = field(default_factory=str)
     use_ip2location: bool = field(default_factory=bool)

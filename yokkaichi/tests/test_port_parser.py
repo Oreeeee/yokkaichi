b04498b6f4ab9b2db8c333a9545e91c1aec5526e
Oreeeee <oree3ee@gmail.com>
2023-05-17 23:53:08 +0200

    Add testing for duplicates in port range

--- yokkaichi/tests/test_port_parser.py
+++ yokkaichi/tests/test_port_parser.py
@@ -5,3 +5,6 @@ def test_parse_port_range():
     assert yokkaichi.port_parser.parse_port_range(
         "25560-25563,25567,25569-25571,49999"
     ) == [25569, 25570, 25571, 49999, 25560, 25561, 25562, 25563, 25567]
+
+def test_parse_port_range_duplicates():
+    assert yokkaichi.port_parser.parse_port_range("25563-25566,25565") == [25563, 25564, 25565, 25566]

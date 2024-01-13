cce48c5de417d891fb51bb2817237bcd1beef406
Oreeeee <oree3ee@gmail.com>
2024-01-13 22:02:59 +0100

    Temporarily remove heatmap

--- yokkaichi_web/map/map.js
+++ yokkaichi_web/map/map.js
@@ -16,18 +16,18 @@ function standardMap() {
   L.marker([0.0, 0.0], { icon: serverIcon }).addTo(map);
 }
 
-function heatmap() {
-  // TODO: add a real heatmap there
-  var map = L.map("heatmap").setView([0.0, 0.0], 1);
+// function heatmap() {
+//   // TODO: add a real heatmap there
+//   var map = L.map("heatmap").setView([0.0, 0.0], 1);
 
-  console.log(serverIcon);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+//   console.log(serverIcon);
+//   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     attribution:
+//       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   }).addTo(map);
 
-  L.marker([0.0, 0.0], { icon: serverIcon }).addTo(map);
-}
+//   L.marker([0.0, 0.0], { icon: serverIcon }).addTo(map);
+// }
 
 standardMap();
-heatmap();
+// heatmap();

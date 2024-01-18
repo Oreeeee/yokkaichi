f71db873714581736aa59c3e1de1690782521d36
Oreeeee <oree3ee@gmail.com>
2024-01-18 20:00:47 +0100

    Add reading IP file

--- yokkaichi_c/main.c
+++ yokkaichi_c/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello World\n");
+int main() {
+    printf("Loading IP List\n");
+
+    FILE *ipList;
+
+    ipList = fopen("data/ips.txt", "r");
+    if (ipList == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    char c = fgetc(ipList);
+    while (c != EOF) {
+        printf("%c", c);
+        c = fgetc(ipList);
+    }
+    fclose(ipList);
+
     return 0;
 }

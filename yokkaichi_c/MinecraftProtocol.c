0d264b830c54fc619c96589f60850f899fe04763
Oreeeee <oree3ee@gmail.com>
2024-03-11 19:52:59 +0100

    Add creating packets and varints

--- yokkaichi_c/MinecraftProtocol.c
+++ yokkaichi_c/MinecraftProtocol.c
@@ -6,6 +6,32 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+void writeVarInt(uint32_t value, char* buffer) {
+    int index = 0;
+    do {
+        char byte = value & SEGMENT_BITS;
+        value >>= 7;
+        if (value != 0) {
+            byte |= CONTINUE_BIT;
+        }
+        buffer[index++] = byte;
+    } while (value != 0 && index < MAX_VARINT_LEN);
+}
+
+void packPingPacket() {
+    MinecraftJavaPacket packet;
+    MinecraftSLP pingPacket;
+
+    // Fill in packet ID for Minecraft packet
+    writeVarInt(PING_PACKET_ID, packet.packetID);
+
+    // Fill in data for ping packet
+    writeVarInt(47, pingPacket.protocolVersion); // We use 47 (1.8) because it's compatible with everything
+    strncpy(pingPacket.serverAddress, "127.0.0.1", sizeof(pingPacket.serverAddress)); // We temporarily use localhost here
+    pingPacket.serverPort = 25565; // We use 25565 here temporarily
+    writeVarInt(1, pingPacket.nextState); // We use 1 here for getting the SLP
+}
+
 void pingJava(char *ip, uint16_t port) {
     // Create socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
--- yokkaichi_c/MinecraftProtocol.h
+++ yokkaichi_c/MinecraftProtocol.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
-#define MAX_VARINT_LEN 5
+#define MAX_VARINT_LEN 6
+#define SEGMENT_BITS 0x7F
+#define CONTINUE_BIT 0x80
+#define PING_PACKET_ID 0
 
 typedef struct {
     char lenght[MAX_VARINT_LEN];
@@ -15,9 +18,10 @@ typedef struct {
     char protocolVersion[MAX_VARINT_LEN];
     char serverAddress[255]; // TODO: Replace this with Unicode string
     uint16_t serverPort;
-    char nextState[MAX_VARINT_LEN];
+    char nextState[MAX_VARINT_LEN]; // Will always be 1 (status) here
 } MinecraftSLP;
 
+void writeVarInt(uint32_t value, char* buffer);
 void pingJava(char *ip, uint16_t port);
 
 #endif
